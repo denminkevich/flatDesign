94ed282bfa87125c6e4688ae853edf59241f1cbb
Denis Minkevich <minkevich2001@gmail.com>
2019-11-08 21:01:39 +0300

    fix: delete slick slider and components add comments for html file

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
 let scrollDown = document.querySelector('.first_display-scroll_down');
 
-// function scrollFunc() {
+function scrollFunc() {
+    window.addEventListener('scroll', function() {
+        let y = window.scrollY;
     
-// }
+        if (y >= 1 & y <= 800) {
+            scrollDown.classList.remove('hideScrollDown');
+            scrollDown.classList.add('showScrollDown');
+        } else {
+            scrollDown.classList.remove('showScrollDown');
+            scrollDown.classList.add('hideScrollDown');
+        }
+    });
+}
 
-window.addEventListener('scroll', function() {
-    let y = window.scrollY;
-
-    if (y >= 1 & y <= 800) {
-        scrollDown.classList.remove('hideScrollDown');
-        scrollDown.classList.add('showScrollDown');
-    } else {
-        scrollDown.classList.remove('showScrollDown');
-        scrollDown.classList.add('hideScrollDown');
-    }
-});
+scrollFunc();
