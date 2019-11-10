0497513c879fbd0cf382c307c5d2e355b1679f2c
Denis Minkevich <minkevich2001@gmail.com>
2019-11-10 13:08:43 +0300

    feat: make self-made slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+//for sroll down button
 let scrollDown = document.querySelector('.first_display-scroll_down');
 
 function scrollFunc() {
@@ -15,3 +17,91 @@ function scrollFunc() {
 }
 
 scrollFunc();
+
+
+//for slider in projects block
+
+function projectsSlider(i) {
+    let sliderRightArr = document.querySelectorAll('.slider_right_arr')[i],
+        firstPic = document.querySelectorAll('.image_one')[i],
+        secondPic = document.querySelectorAll('.image_two')[i],
+        thirdPic = document.querySelectorAll('.image_three')[i],
+        picOneOrder = 1,
+        picTwoOrder = 2,
+        picThreeOrder = 3;
+        sliderRightArr.addEventListener('click', function() {
+
+            if (picOneOrder < 3) {
+                picOneOrder++;
+            } else {
+                picOneOrder = 1;
+            }
+            
+            switch(picOneOrder) {
+                case 1:
+                    firstPic.style.order = "1";
+                    break;
+                case 2:
+                    firstPic.style.order = "2";
+                    break;
+                case 3: 
+                    firstPic.style.order = "3";
+                    break;
+
+                default:
+                    firstPic.style.order = "1";
+                    
+            }
+
+            if (picTwoOrder < 3) {
+                picTwoOrder++;
+            } else {
+                picTwoOrder = 1;
+            }
+            
+            switch(picTwoOrder) {
+                case 1:
+                    secondPic.style.order = "1";
+                    break;
+                case 2:
+                    secondPic.style.order = "2";
+                    break;
+                case 3: 
+                    secondPic.style.order = "3";
+                    break;
+
+                default:
+                    secondPic.style.order = "1";
+                    
+            }
+
+            if (picThreeOrder < 3) {
+                picThreeOrder++;
+            } else {
+                picThreeOrder = 1;
+            }
+            
+            switch(picThreeOrder) {
+                case 1:
+                    thirdPic.style.order = "1";
+                    break;
+                case 2:
+                    thirdPic.style.order = "2";
+                    break;
+                case 3: 
+                    thirdPic.style.order = "3";
+                    break;
+
+                default:
+                    thirdPic.style.order = "1";
+                    
+            }
+    });
+}
+
+projectsSlider(0);
+projectsSlider(1);
+projectsSlider(2);
+
+// tabs in project block
+
